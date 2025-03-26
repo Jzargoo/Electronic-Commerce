d5bc3b8476f9a7ad75f59fd06c8af8cd773e4a31
Zhandos <z.ishanov04@gmail.com>
2025-03-26 20:25:09 +0500

    Add new entities, update repositories, and implement services for cart and order management

--- BuySmart-services/src/main/java/com/jzargo/dto/AddressDto.java
+++ BuySmart-services/src/main/java/com/jzargo/dto/AddressDto.java
@@ -3,7 +3,9 @@ package com.jzargo.dto;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
+import lombok.Data;
 
+@Data
 public class AddressDto {
 
     @NotEmpty(message = "Street cannot be empty")
---
+++ BuySmart-services/src/main/java/com/jzargo/mapper/AddressReadMapper.java
@@ -0,0 +1,20 @@
+package com.jzargo.mapper;
+
+import com.jzargo.dto.AddressDto;
+import com.jzargo.entity.Address;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AddressReadMapper implements Mapper<Address, AddressDto> {
+
+    @Override
+    public AddressDto map(Address address) {
+        AddressDto addressDto = new AddressDto();
+        addressDto.setStreet(address.getStreet());
+        addressDto.setCity(address.getCity());
+        addressDto.setState(address.getState());
+        addressDto.setCountry(address.getCountry());
+        addressDto.setZipCode(address.getZipCode());
+        return addressDto;
+    }
+}
